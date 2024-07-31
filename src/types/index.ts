a996a049e3e2485fb95cd23ef5e393e670539919
Ilzarich <IlyaTuev1551@yandex.ru>
2024-07-31 13:59:11 +0300

    Update file

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,47 +1,42 @@
-// Карточка товара 
 export interface IProductItem {
     id: string;
-    description: string;
-    image: string;
     title: string;
+    description: string;
     category: string;
-    price: number;
-}
-
-// Каталог товаров  
-export interface IProductList {
-    total: number;
-    items: IProductItem[];
-}
-
-// Модальное окно форма оплаты  и адресса 
-export interface IAddressForm {
-    payment: 'online' | 'upon receipt';
-    address: string;
-}
-
-// Модальное окно форма контактов
-export interface IContactForm { 
-    mail: string;
-    phone: number;
-}
-
-// Модальное окно после оформления заказа
-export interface IOrderResult {
+    image: string;
+    price: number | null;
+  }
+  
+  export interface IAppState {
+    catalog: IProductItem[];
+    preview: string;
+    basket: string[];
+    order: IOrder;
+    total: string | number;
+    loading: boolean;
+  }
+  
+  
+  export interface IProductsList {
+    products: IProductItem[];
+  }
+  
+  
+  export interface IOrderForm {
+    payment?: string;
+    address?: string;
+    phone?: string;
+    email?: string;
+    total?: string | number;
+  }
+  
+  export interface IOrder extends IOrderForm {
+    items: string[];
+  }
+  
+  
+  export type FormErrors = Partial<Record<keyof IOrder, string>>;
+  
+  export interface IOrderResult {
     id: string;
-    total: number;
-}
-
-// Тип данных с ошибкой в адресе доставки 
-export type AddressFormError = Partial<Record<keyof IAddressForm, string>>;
-
-// Тип данных с ошибкой в контактах
-export type ContactFormError = Partial<Record<keyof IContactForm, string>>;
-
-
-
-
-//
-export interface IError {
-    error: string;
-}
+  }
